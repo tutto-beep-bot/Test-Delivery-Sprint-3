9cd09bf627d5cdaae7919f9d567b50418817c64b
Davi Lessa <davilessa.br@gmail.com>
2025-03-06 10:17:24 +0100

    Ex. 1 - Function with Array off all directors done.

--- src/films.js
+++ src/films.js
@@ -1,6 +1,6 @@
 // Exercise 1: Get the array of all directors.
-function getAllDirectors(array) {
-  let result =  ???;
+function getAllDirectors(movies) {
+  let result = movies.map((movie) => movie.director);
   console.log("EXERCICE 1 ->", result);
   return result;
 }
